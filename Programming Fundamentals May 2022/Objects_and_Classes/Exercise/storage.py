020dab55d8f79da8a3d6279a3a276ed41352f1da
Mediha Kehayova <99451787+merakeh@users.noreply.github.com>
2022-10-01 02:23:10 +0300

    Exercise on Objects and Classes

---
+++ Programming Fundamentals May 2022/Objects_and_Classes/Exercise/storage.py
@@ -0,0 +1,25 @@
+
+class Storage:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.storage = []
+
+    def add_product(self, product: str):
+        if len(self.storage) >= self.capacity:
+            pass
+        else:
+            self.storage.append(product)
+
+    def get_products(self):
+        return self.storage
+
+
+storage = Storage(3)
+storage.add_product("apple")
+storage.add_product("banana")
+storage.add_product("potato")
+storage.add_product("tomato")
+storage.add_product("bread")
+print(storage.get_products())
+
+
