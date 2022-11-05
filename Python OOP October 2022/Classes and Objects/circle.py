358f73d0e07f35c2b8198829bf839345378807b9
Mediha Kehayova <99451787+merakeh@users.noreply.github.com>
2022-11-05 02:24:15 +0200

    Classes and Objects
    Lab + Exercise

---
+++ Python OOP October 2022/Classes and Objects/circle.py
@@ -0,0 +1,51 @@
+import unittest
+
+
+class Circle:
+    pi = 3.14
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def set_radius(self, new_radius):
+        self.radius = new_radius
+
+    def get_area(self):
+        return Circle.pi * self.radius ** 2
+
+    def get_circumference(self):
+        return 2 * Circle.pi * self.radius
+
+
+circle = Circle(10)
+circle.set_radius(12)
+print(circle.get_area())
+print(circle.get_circumference())
+
+
+#
+#
+# class Tests(unittest.TestCase):
+#     def test_init(self):
+#         c = Circle(15)
+#         self.assertEqual(c.radius, 15)
+#
+#     def test_class_attributes(self):
+#         self.assertEqual(Circle.pi, 3.14)
+#
+#     def test_set_radius(self):
+#         c = Circle(15)
+#         c.set_radius(160)
+#         self.assertEqual(c.radius, 160)
+#
+#     def test_get_area(self):
+#         c = Circle(4)
+#         self.assertEqual(c.get_area(), 50.24)
+#
+#     def test_get_circumference(self):
+#         c = Circle(11)
+#         self.assertEqual(c.get_circumference(), 69.08)
+#
+#
+# if __name__ == "__main__":
+#     unittest.main()
